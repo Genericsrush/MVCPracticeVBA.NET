ee1f71357ade95422e8c1a93159d3f3cc4a4f650
GenericsRush <Macray607@hotmail.com>
2021-01-12 15:24:54 -0600

    Added User model

---
+++ MVCControllerDemo/Models/User.vb
@@ -0,0 +1,14 @@
+﻿Imports System.Data.Entity
+
+Public Class User
+    Public Property Id As Integer
+    Public Property Username As Integer
+    Public Property CreationDate As DateTime
+    Public Property Password As String
+    Public Property Email As String
+
+End Class
+Public Class UserDBContent
+    Inherits DbContext
+    Public Property Users() As DbSet(Of User)
+End Class
